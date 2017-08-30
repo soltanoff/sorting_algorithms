90aea49369e87d5a68306403807ac2d557190953
soltanoff <piccadillable@gmail.com>
2017-08-30 18:06:49 +0300

    refactoring sort_by_merge

--- by_merge.py
+++ by_merge.py
@@ -28,26 +28,24 @@ def sort_by_merge(array: list) -> list:
         sort_by_merge(left_half)
         sort_by_merge(right_half)
 
-        i = 0
-        j = 0
-        k = 0
+        i = j = k = 0
         while i < len(left_half) and j < len(right_half):
             if left_half[i] < right_half[j]:
                 array[k] = left_half[i]
-                i = i + 1
+                i += 1
             else:
                 array[k] = right_half[j]
-                j = j + 1
-            k = k + 1
+                j += 1
+            k += 1
 
         while i < len(left_half):
             array[k] = left_half[i]
-            i = i + 1
-            k = k + 1
+            i += 1
+            k += 1
 
         while j < len(right_half):
             array[k] = right_half[j]
-            j = j + 1
-            k = k + 1
+            j += 1
+            k += 1
 
     return array
